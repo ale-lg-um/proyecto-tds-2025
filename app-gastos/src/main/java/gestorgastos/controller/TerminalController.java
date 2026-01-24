a24c98252c9ef25659a98e97c0343483020c2f24
SpiderYoda11 <galatasari1970@gmail.com>
2026-01-24 13:54:48 +0100

    Corregidos PRs 6-7

--- app-gastos/src/main/java/gestorgastos/controller/TerminalController.java
+++ app-gastos/src/main/java/gestorgastos/controller/TerminalController.java
@@ -10,6 +10,9 @@ import javafx.scene.control.TextField;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
 
 
 public class TerminalController {
@@ -24,7 +27,11 @@ public class TerminalController {
 
     // MÁQUINA DE ESTADOS
     // 0=Cmd, 1=Concepto, 2=Importe, 3=Fecha, 4=Categoría, 5=Pagador
-    private int pasoActual = 0; 
+    //private int pasoActual = 0;
+    
+    // Corrección sexto PR
+    private EstadoTerminal pasoActual = EstadoTerminal.CMD;
+    private Map<EstadoTerminal, Consumer<String>> accionesPorEstado;
     
     // Variables temporales
     private String tempConcepto;
@@ -48,6 +55,21 @@ public class TerminalController {
     @FXML
     public void initialize() {
         Platform.runLater(() -> txtInput.requestFocus());
+        configurarMaquinaDeEstados();
+    }
+    
+    //Corrección sexto PR
+    private void configurarMaquinaDeEstados() {
+    	this.accionesPorEstado = new HashMap<>();
+    	
+    	accionesPorEstado.put(EstadoTerminal.CMD, this::ejecutarComandoPrincipal);
+    	accionesPorEstado.put(EstadoTerminal.CONCEPTO, this::procesarConcepto);
+    	accionesPorEstado.put(EstadoTerminal.IMPORTE, this::procesarImporte);
+    	accionesPorEstado.put(EstadoTerminal.FECHA, this::procesarFecha);
+    	accionesPorEstado.put(EstadoTerminal.HORA, this::procesarHora);
+    	accionesPorEstado.put(EstadoTerminal.CATEGORIA, this::procesarCategoria);
+    	accionesPorEstado.put(EstadoTerminal.PAGADOR, this::procesarPagador);
+    	accionesPorEstado.put(EstadoTerminal.BORRAR, this::procesarBorrar);
     }
 
     @FXML
@@ -56,10 +78,24 @@ public class TerminalController {
         txtInput.clear();
         txtOutput.appendText(texto + "\n"); // Eco
 
-        if (pasoActual == 0) {
+        /*if (pasoActual == 0) {
             ejecutarComandoPrincipal(texto.toLowerCase());
         } else {
             procesarPasoRegistro(texto);
+        }*/
+        
+        // Corrección sexto PR
+        try {
+        	if(accionesPorEstado.containsKey(pasoActual)) {
+        		accionesPorEstado.get(pasoActual).accept(texto);
+        	} else {
+        		imprimir("ERROR: Estado no admitido");
+        		pasoActual = EstadoTerminal.CMD;
+        	}
+        } catch (Exception e) {
+        	imprimir("ERROR: " + e.getMessage());
+        	pasoActual = EstadoTerminal.CMD;
+        	reiniciarPrompt();
         }
     }
 
@@ -77,13 +113,13 @@ public class TerminalController {
                 reiniciarPrompt();
                 break;
             case "registrar":
-                pasoActual = 1;
+                pasoActual = EstadoTerminal.CONCEPTO;
                 imprimir("--- Nuevo Gasto ---");
                 imprimir("Introduce el Concepto:");
                 break;
             case "borrar":
                 listarGastos();
-                pasoActual = 10;
+                pasoActual = EstadoTerminal.BORRAR;
                 imprimir("Introduce el ID del gasto a borrar:");
                 break;
             case "clear":
@@ -96,7 +132,7 @@ public class TerminalController {
         }
     }
 
-    private void procesarPasoRegistro(String entrada) {
+    /*private void procesarPasoRegistro(String entrada) {
         try {
             switch (pasoActual) {
                 case 1:
@@ -187,52 +223,144 @@ public class TerminalController {
             pasoActual = 0; 
             reiniciarPrompt();
         }
+    }*/
+    
+    // Corrección sexto PR
+    private void procesarConcepto(String entrada) {
+    	if(entrada.isEmpty()) throw new RuntimeException("El concepto no puede ser nulo");
+    	tempConcepto = entrada;
+    	pasoActual = EstadoTerminal.IMPORTE;
+    	imprimir("Introduce el importe: ");
+    }
+    
+    private void procesarImporte(String entrada) {
+    	try {
+    		tempImporte = Double.parseDouble(entrada.replace(",", "."));
+    		pasoActual = EstadoTerminal.FECHA;
+    		imprimir("Fecha (AAAA-MM-DD) [Enter para Hoy]: ");
+    	} catch (NumberFormatException e) {
+    		throw new RuntimeException("Número Inválido");
+    	}
+    }
+    
+    private void procesarFecha (String entrada) {
+    	if(entrada.isEmpty()) {
+    		tempFecha = LocalDate.now();
+    	} else {
+    		try {
+    			tempFecha = LocalDate.parse(entrada);
+    		} catch (DateTimeParseException e) {
+    			throw new RuntimeException("Formato incorrecto. Usa AAAA-MM-DD");
+    		}
+    	}
+    	pasoActual = EstadoTerminal.HORA;
+    	imprimir("Hora (HH:mm) [Enter para Ahora]: ");
+    }
+    
+    private void procesarHora(String entrada) {
+    	if(entrada.isEmpty()) {
+    		tempHora = java.time.LocalTime.now();
+    	} else {
+    		try {
+    			tempHora = java.time.LocalTime.parse(entrada);
+    		} catch (DateTimeParseException e) {
+    			throw new RuntimeException("Formato incorreco. Usa HH:mm");
+    		}
+    	}
+    	pasoActual = EstadoTerminal.CATEGORIA;
+    	imprimir("Categoría (o Enter para General): ");
+    }
+    
+    private void procesarCategoria(String entrada) {
+    	String catNombre = entrada;
+    	Categoria cat = cuentaActiva.getCategorias().stream()
+    			.filter(c -> c.getNombre().equalsIgnoreCase(catNombre))
+    			.findFirst()
+    			.orElse(cuentaActiva.getCategorias().get(0));
+    	tempCategoria = cat.getNombre();
+    	
+    	if(cuentaActiva instanceof CuentaCompartida) {
+    		pasoActual = EstadoTerminal.PAGADOR;
+    		imprimir("Pagador (nombre): ");
+    	} else {
+    		guardarGasto(cat, "Yo");
+    	}
+    }
+    
+    private void procesarPagador(String entrada) {
+    	if(entrada.isEmpty()) {
+    		throw new RuntimeException("El pagador no puede ser nulo");
+    	} else {
+    		Categoria cat = cuentaActiva.getCategorias().stream()
+    				.filter(c -> c.getNombre().equalsIgnoreCase(tempCategoria))
+    				.findFirst()
+    				.orElse(cuentaActiva.getCategorias().get(0));
+    		guardarGasto(cat, entrada);
+    	}
+    }
+    
+    private void procesarBorrar(String entrada) {
+    	try {
+    		int id = Integer.parseInt(entrada);
+    		if((id >= 0) && (id < cuentaActiva.getGastos().size())) {
+    			Gasto g = cuentaActiva.getGastos().remove(id);
+    			guardarCambios();
+    			imprimir("✓ Borrado: " + g.getConcepto()); 
+    		} else {
+    			imprimir("ERROR: id inválido.");
+    		}
+    	} catch (NumberFormatException e) {
+    		imprimir("ERROR: id inválido");
+    	}
+    	pasoActual = EstadoTerminal.CMD;
+    	reiniciarPrompt();
     }
 
     private void guardarGasto(Categoria cat, String pagador) {
-    // Crear el gasto
-    Gasto nuevo = new Gasto(tempConcepto, tempImporte, tempFecha, cat, pagador);
+    	// Crear el gasto
+    	Gasto nuevo = new Gasto(tempConcepto, tempImporte, tempFecha, cat, pagador);
     
-    // Editar la hora
-    nuevo.setHora(tempHora);
+    	// Editar la hora
+    	nuevo.setHora(tempHora);
 
-    // Comprobar alertas
-    gestorgastos.services.ServicioAlertas servicio = new gestorgastos.services.ServicioAlertas();
+    	// Comprobar alertas
+    	//gestorgastos.services.ServicioAlertas servicio = new gestorgastos.services.ServicioAlertas();
     
-    // Guardamos el resultado. Si hay alerta, el servicio ya creó la notificación internamente.
-    //String mensajeError = servicio.comprobarAlertas(cuentaActiva, nuevo); 
+    	// Guardamos el resultado. Si hay alerta, el servicio ya creó la notificación internamente.
+    	//String mensajeError = servicio.comprobarAlertas(cuentaActiva, nuevo); 
 
-    // Si mensajeError tiene texto, significa que nos hemos pasado del límite de la alerta
+    	// Si mensajeError tiene texto, significa que nos hemos pasado del límite de la alerta
     
-    // Corrección tercer PR
-    Alerta alertaSaltada = servicio.comprobarAlertas(cuentaActiva, nuevo);
-    if (alertaSaltada != null) {
-    	String mensaje = "Has superado el límite de " + alertaSaltada.getLimite() + "€ definido en tu alerta.";
-        // Como estamos en CMD, no puede aparecer una ventana con el mensaje, así que el mensaje se imprime por la terminal
-        imprimir("\n**************************************************");
-        imprimir("⚠️  GASTO BLOQUEADO POR ALERTA");
-        imprimir("--------------------------------------------------");
-        imprimir(mensaje);
-        imprimir("--------------------------------------------------");
-        imprimir("ℹ️  Se ha generado una notificación en tu cuenta.");
-        imprimir("    El gasto SI se ha guardado.");
-        imprimir("**************************************************\n");
+    	// Corrección tercer PR
+    	Alerta alertaSaltada = cuentaService.agregarGasto(cuentaActiva, nuevo);
+    	if (alertaSaltada != null) {
+    		String mensaje = "Has superado el límite de " + alertaSaltada.getLimite() + "€ definido en tu alerta.";
+    		// Como estamos en CMD, no puede aparecer una ventana con el mensaje, así que el mensaje se imprime por la terminal
+    		imprimir("\n**************************************************");
+    		imprimir("⚠️  GASTO BLOQUEADO POR ALERTA");
+    		imprimir("--------------------------------------------------");
+    		imprimir(mensaje);
+    		imprimir("--------------------------------------------------");
+    		imprimir("ℹ️  Se ha generado una notificación en tu cuenta.");
+    		imprimir("    El gasto SI se ha guardado.");
+    		imprimir("**************************************************\n");
 
-        // Reseteamos el flujo para volver al menú principal
-        guardarCambios();
-        pasoActual = 0;
-        reiniciarPrompt();
+    		// Reseteamos el flujo para volver al menú principal
+    		//guardarCambios();
+    		//pasoActual = EstadoTerminal.CMD;
+    		//reiniciarPrompt();
         
-    }
+    	}
 
-    // Guardamos el gasto
-    cuentaActiva.agregarGasto(nuevo);
-    guardarCambios();
+    	// Guardamos el gasto
+    	//cuentaActiva.agregarGasto(nuevo);
+    	//guardarCambios();
     
-    imprimir("✓ Gasto guardado (" + tempFecha + " " + tempHora.toString().substring(0,5) + ").");
-    pasoActual = 0;
-    reiniciarPrompt();
-}
+    	imprimir("✓ Gasto guardado (" + tempFecha + " " + tempHora.toString().substring(0,5) + ").");
+    	if(onUpdateAction != null) onUpdateAction.run();
+    	pasoActual = EstadoTerminal.CMD;
+    	reiniciarPrompt();
+    }
     
     private void guardarCambios() {
         cuentaService.agregarCuenta(cuentaActiva);
--- app-gastos/src/main/java/gestorgastos/services/CuentaService.java
+++ app-gastos/src/main/java/gestorgastos/services/CuentaService.java
@@ -1,6 +1,8 @@
 package gestorgastos.services;
 
+import gestorgastos.model.Alerta;
 import gestorgastos.model.Cuenta;
+import gestorgastos.model.Gasto;
 import gestorgastos.model.Usuario;
 import gestorgastos.repository.CuentaRepository;
 import gestorgastos.repository.CuentaRepositoryJson;
@@ -49,5 +51,20 @@ public class CuentaService {
     // Eliminar una cuenta
     public void eliminarCuenta(Cuenta cuenta) {
         repositorio.delete(cuenta);
+    }
+    
+    // Corrección séptimo PR
+    public Alerta agregarGasto(Cuenta cuenta, Gasto nuevo) {
+    	cuenta.agregarGasto(nuevo);
+    	
+    	ServicioAlertas servicio = new ServicioAlertas();
+    	Alerta saltada = servicio.comprobarAlertas(cuenta, nuevo);
+    	
+    	if(saltada != null) {
+    		String mensaje = "Has superado el límite de " + saltada.getLimite() + "€ establecido en la alerta";
+    		cuenta.anadirNotificacion(mensaje);
+    	}
+    	agregarCuenta(cuenta);
+    	return saltada;
     }
 }
